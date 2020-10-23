683577725a6417e51a7db1953e060eeb1fb59ac9
DimasBagasAjipratama <56101230+DimasBagasAjipratama@users.noreply.github.com>
2020-10-23 22:49:25 +0700

    Edit tampilan

--- Employee/Startup.cs
+++ Employee/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace Employee
 {
@@ -33,6 +35,9 @@ namespace Employee
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<Models.EmployeeContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
